da3c9b0a79ef5a8ce53326aa761809bedb775fc7
RussWong <37031190+RussWong@users.noreply.github.com>
2025-03-21 15:10:11 +0800

    Update HF_llama_run_script.py

--- tools/HF_llama_run_script.py
+++ tools/HF_llama_run_script.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, LlamaForCausalLM
-model = LlamaForCausalLM.from_pretrained("/home/gyou_llama/llama/out_7B")
-tokenizer = AutoTokenizer.from_pretrained("/home/gyou_llama/llama/out_7B")
+# 注意，这个py用来debug的，作为本课程各个kernel的groudtruth，且此huggingface接口只接受Llama-2-7b-hf，不接受Llama-2-7b
+model = LlamaForCausalLM.from_pretrained("/path/to/Llama-2-7b-hf")
+tokenizer = AutoTokenizer.from_pretrained("/path/to/Llama-2-7b-hf")
 prompt = "Hey, are you conscious? Can you talk to me?"
 inputs = tokenizer(prompt, return_tensors="pt")
 generate_ids = model.generate(inputs.input_ids, max_length=30)
